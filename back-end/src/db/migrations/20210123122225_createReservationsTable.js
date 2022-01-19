bf0be1e97cded1734654ba0c84247ab9f2579e08
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-19 10:35:47 -0800

    added strings to knex reservations table

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,6 +1,27 @@
+
+// Example data structure to add
+// {
+//   "first_name": "Rick",
+//   "last_name": "Sanchez",
+//   "mobile_number": "202-555-0164",
+//   "reservation_date": "2020-12-31",
+//   "reservation_time": "20:00:00",
+//   "people": 6,
+//   "created_at": "2020-12-10T08:30:32.326Z",
+//   "updated_at": "2020-12-10T08:30:32.326Z"
+// },
+
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
+    // Add 
+    table.string("first_name")
+    table.string("last_name");
+    table.string("mobile_number");
+    table.string("reservation_date");
+    table.string("reservation_time");
+    table.string("people");
+
     table.timestamps(true, true);
   });
 };
