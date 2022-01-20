2cb8d9b955f343775800186d639c3ec80c964ec6
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-20 11:47:29 -0800

    orderBy on list

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,11 +1,11 @@
 const knex = require("../db/connection");
 
 function create(newReservation) {
-  return knex("reservations").insert(newReservation).returning("*");
+  return knex("reservations").insert(newReservation).returning("*").then(reservations => reservations[0])
 }
 
 async function list() {
-  return knex("reservations").select("*");
+  return knex("reservations").select("*").orderBy("reservation_time");
 }
 
 module.exports = {
