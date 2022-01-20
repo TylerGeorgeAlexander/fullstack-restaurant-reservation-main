6fc833eaf99ee544e6365140d91084a269a932fd
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-20 13:11:32 -0800

    filtered table with .where = date argument

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,11 +1,17 @@
 const knex = require("../db/connection");
 
 function create(newReservation) {
-  return knex("reservations").insert(newReservation).returning("*").then(reservations => reservations[0])
+  return knex("reservations")
+    .insert(newReservation)
+    .returning("*")
+    .then((reservations) => reservations[0]);
 }
 
-async function list() {
-  return knex("reservations").select("*").orderBy("reservation_time");
+async function list(date) {
+  return knex("reservations")
+    .select("*")
+    .orderBy("reservation_time")
+    .where({ reservation_date: date });
 }
 
 module.exports = {
