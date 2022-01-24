15a0e43ed7a07ed9c27359f27f126d25dc01e985
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-24 09:01:48 -0800

    added foreign keys and references to migration

--- back-end/src/db/migrations/20220124081858_createTablesTable.js
+++ back-end/src/db/migrations/20220124081858_createTablesTable.js
@@ -1,15 +1,21 @@
 exports.up = function (knex) {
-    return knex.schema.createTable("tables", (table) => {
-      table.increments("table_id").primary();
-      // Add 
-      table.string("table_name")
-      table.integer("capacity");
-  
-      table.timestamps(true, true);
-    });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTable("tables");
-  };
-  
+  return knex.schema.createTable("tables", (table) => {
+    table.increments("table_id").primary();
+    // Add
+    table.string("table_name");
+    table.integer("capacity");
+
+    // Reference
+    table.integer("reservation_id");
+    table
+      .foreign("reservation_id")
+      .references("reservation_id")
+      .inTable("reservations")
+      .onDelete("cascade");
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("tables");
+};
