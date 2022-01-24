a77aa2d9e506f8a87856db9a78786d66abbb5ac0
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-24 11:26:19 -0800

    added validation middleware

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -31,11 +31,27 @@ function hasData(req, res, next) {
   next({ status: 400, message: "body must have data property" });
 }
 
+function hasTableName(req, res, next) {
+  if (req.body.data.table_name && req.body.data.table_name.length > 1) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "table_name is missing, empty, or one character",
+  });
+}
+
+function hasCapacity(req, res, next) {
+  if (req.body.data.capacity && typeof req.body.data.capacity === "number") {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "capacity is missing, zero, or not a number",
+  });
+}
 
 module.exports = {
-    create: [
-      hasData,
-      asyncErrorBoundary(create),
-    ],
-    list: asyncErrorBoundary(list),
-  };
+  create: [hasData, hasTableName, hasCapacity, asyncErrorBoundary(create)],
+  list: asyncErrorBoundary(list),
+};
