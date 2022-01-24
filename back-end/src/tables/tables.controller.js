d03608b8b706715de71b0660051ac51543ecf27e
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-24 08:39:18 -0800

    removed passed props in list and commented out ex

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -5,8 +5,8 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
  * List handler for reservation resources
  */
 async function list(req, res) {
-  const { date } = req.query;
-  const data = await service.list(date);
+  // const { date } = req.query;
+  const data = await service.list();
   res.json({
     data,
   });
