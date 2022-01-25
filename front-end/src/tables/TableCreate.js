10005f0d82bb6ca63b0ddb94e1ce76c59a0d689c
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-25 08:06:20 -0800

    created TableChange to lift state and pass props

---
+++ front-end/src/tables/TableCreate.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import TableForm from "./TableForm";
+import { useHistory } from "react-router-dom";
+import { createTable } from "../utils/api";
+
+export default function TableCreate() {
+  let initialState = {
+    table_name: "",
+    capacity: "",
+  };
+
+  const [table, setTable] = useState(initialState);
+  const [error, setError] = useState(null);
+
+  const history = useHistory();
+
+  function cancelHandler(event) {
+    // console.log("cancel test");
+    event.preventDefault();
+    history.push("/");
+  }
+
+  function submitHandler(event) {
+    // console.log("submit test");
+    event.preventDefault();
+    // console.log(table)
+    createTable(table)
+      .then(() => {
+        history.push(`/dashboard/date?date=${table.table_id}`);
+      })
+      .catch(setError);
+  }
+
+  function changeHandler({ target: { name, value } }) {
+    setTable((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  function changeNumberHandler({ target: { name, value } }) {
+    setTable((prevState) => ({
+      ...prevState,
+      [name]: Number(value),
+    }));
+  }
+
+  return (
+    <TableForm
+      table={table}
+      error={error}
+      submitHandler={submitHandler}
+      cancelHandler={cancelHandler}
+      changeHandler={changeHandler}
+      changeNumberHandler={changeNumberHandler}
+    />
+  );
+}
