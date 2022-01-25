295fe89e754ddb72782852fa61d0393e7bd3b935
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-25 12:43:48 -0800

    Free and Finish Button for US-05

--- front-end/src/dashboard/TablesTable.js
+++ front-end/src/dashboard/TablesTable.js
@@ -1,29 +1,34 @@
 export default function TablesTable({ tables }) {
-    return (
-      <table className="table table-striped table-dark">
-        <thead>
-          <tr>
-            <th scope="col">Table ID #</th>
-            <th scope="col">Table Name</th>
-            <th scope="col">Capacity</th>
-            <th scope="col">Free?</th>
-            <th scope="col">Button</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tables.map((table) => {
-            return (
-              <tr>
-                <th scope="row">{table.table_id}</th>
-                <td>{table.table_name}</td>
-                <td>{table.capacity}</td>
-                <td>{table.free}</td>
-                <td>{table.button}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    );
-  }
-  
+  return (
+    <table className="table table-striped table-dark">
+      <thead>
+        <tr>
+          <th scope="col">Table ID #</th>
+          <th scope="col">Table Name</th>
+          <th scope="col">Capacity</th>
+          <th scope="col">Free?</th>
+          <th scope="col">Button</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tables.map((table) => {
+          return (
+            <tr>
+              <th scope="row">{table.table_id}</th>
+              <td>{table.table_name}</td>
+              <td>{table.capacity}</td>
+              <td>{table.reservation_id || "Free"}</td>
+              <td>
+                {!table.reservation_id || (
+                  <button>
+                    Finish
+                  </button>
+                )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
