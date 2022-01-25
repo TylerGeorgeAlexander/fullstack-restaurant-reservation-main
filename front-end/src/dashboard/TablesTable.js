3e3a0a558e2e583b71a0e622782f8398d67026c6
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-25 12:00:19 -0800

    TablesTable component to clean html on Dashboard

---
+++ front-end/src/dashboard/TablesTable.js
@@ -0,0 +1,29 @@
+export default function TablesTable({ tables }) {
+    return (
+      <table className="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Table ID #</th>
+            <th scope="col">Table Name</th>
+            <th scope="col">Capacity</th>
+            <th scope="col">Free?</th>
+            <th scope="col">Button</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tables.map((table) => {
+            return (
+              <tr>
+                <th scope="row">{table.table_id}</th>
+                <td>{table.table_name}</td>
+                <td>{table.capacity}</td>
+                <td>{table.free}</td>
+                <td>{table.button}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+  
