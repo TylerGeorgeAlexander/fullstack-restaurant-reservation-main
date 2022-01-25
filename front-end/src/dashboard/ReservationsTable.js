bcba9d5381634a768f8717a5c24721ca9a37b520
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-25 11:56:01 -0800

    seperatd ReservationsTable

---
+++ front-end/src/dashboard/ReservationsTable.js
@@ -0,0 +1,32 @@
+export default function ReservationsTable({ reservations }) {
+  return (
+    <table className="table table-striped table-dark">
+      <thead>
+        <tr>
+          <th scope="col">Reservation ID #</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Telephone #</th>
+          <th scope="col">Reservation Date</th>
+          <th scope="col">Reservation Time</th>
+          <th scope="col"># of People</th>
+        </tr>
+      </thead>
+      <tbody>
+        {reservations.map((reservation) => {
+          return (
+            <tr>
+              <th scope="row">{reservation.reservation_id}</th>
+              <td>{reservation.first_name}</td>
+              <td>{reservation.last_name}</td>
+              <td>{reservation.mobile_number}</td>
+              <td>{reservation.reservation_date}</td>
+              <td>{reservation.reservation_time}</td>
+              <td>{reservation.people}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
