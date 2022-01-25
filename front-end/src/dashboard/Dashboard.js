9b03284d7ade3dacc36529b56e0b908df5c94505
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-25 09:48:10 -0800

    forgot to add buttons from US-01

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import { useHistory } from "react-router-dom";
+
+import { previous, next } from "../utils/date-time";
+import { todaysDayDate } from "../utils/formatted-dates";
 
 /**
  * Defines the dashboard page.
@@ -11,6 +15,8 @@ import ErrorAlert from "../layout/ErrorAlert";
 function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
+
+  const history = useHistory();
 
   useEffect(loadDashboard, [date]);
 
@@ -23,11 +29,62 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  function clickTodayHandler() {
+    let today = todaysDayDate();
+    // console.log(today);
+    history.push(`/dashboard/date?date=${today}`);
+  }
+  function clickPreviousDayHandler() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get("date");
+
+    let prevDay = previous(myParam);
+    // console.log(prevDay);
+    history.push(`/dashboard/date?date=${prevDay}`);
+  }
+  function clickNextDayHandler() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get("date");
+
+    let nextDay = next(myParam);
+    // console.log(nextDay);
+    history.push(`/dashboard/date?date=${nextDay}`);
+  }
+
   return (
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Reservations for date</h4>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={clickPreviousDayHandler}
+          >
+            Previous
+          </button>
+        </div>
+        <div className="col">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={clickTodayHandler}
+          >
+            Today
+          </button>
+        </div>
+        <div className="col">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={clickNextDayHandler}
+          >
+            Next
+          </button>
+        </div>
       </div>
       <ErrorAlert error={reservationsError} />
       {JSON.stringify(reservations)}
