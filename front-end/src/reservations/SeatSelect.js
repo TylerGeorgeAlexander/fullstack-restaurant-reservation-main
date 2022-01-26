de56a22c0224f5aede7804f26f9ec34a50742874
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-26 12:59:32 -0800

    handlers, fetch in useEffect, form

--- front-end/src/reservations/SeatSelect.js
+++ front-end/src/reservations/SeatSelect.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { getReservation } from "../utils/api";
+import { getReservation, listTables, updateTable } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 export default function SeatSelect() {
   const [reservation, setReservation] = useState([]);
+  const [tables, setTables] = useState([]);
+  const [table, setTable] = useState("");
 
+  const [error, setError] = useState(null);
   const [reservationError, setReservationError] = useState(null);
+  const [tablesError, setTablesError] = useState(null);
 
   const { reservation_id } = useParams();
+  const history = useHistory();
 
   useEffect(loadReservation, [reservation_id]);
 
@@ -18,22 +23,65 @@ export default function SeatSelect() {
     getReservation(reservation_id, abortController.signal)
       .then(setReservation)
       .catch(setReservationError);
+    listTables(abortController.signal).then(setTables).catch(setTablesError);
     return () => abortController.abort();
   }
 
-  console.log(reservation);
+  function cancelHandler(event) {
+    // console.log("cancel test");
+    event.preventDefault();
+    history.go(-1);
+  }
+
+  function changeHandler({ target: {  value } }) {
+    setTable(() => ({
+     value,
+    }));
+    // console.log(table) maybe race condition
+  }
+
+  function submitHandler(event) {
+    // console.log("submit test");
+    event.preventDefault();
+    console.log(table.value)
+    updateTable(table.value, reservation.reservation_id)
+      .then(() => {
+        history.push(`/dashboard/date?date=${reservation.reservation_date}`);
+      })
+      .catch(setError);
+  }
 
   return (
     <div>
-      <h2>Seat Reservation</h2>
-      {reservation.reservation_id}
-      <select className="form-control" id="locale">
-        <option value="af-ZA">af-ZA</option>
-        <option value="af-ZA">af-ZA2</option>
-      </select>
-      <button>Cancel</button>
-      <button>Submit</button>
-      <ErrorAlert error={reservationError} />
+      <form onSubmit={submitHandler}>
+        <h2>Seat Reservation</h2>
+        <h4>
+          {reservation.reservation_id} - {reservation.first_name}{" "}
+          {reservation.last_name} on {reservation.reservation_date} at{" "}
+          {reservation.reservation_time} for {reservation.people}
+        </h4>
+        <select className="table_id" id="locale" onChange={changeHandler}>
+          <option>Select a table</option>
+          {tables.map((table) => {
+            return (
+              <option key={table.table_id} value={table.table_id}>
+                {table.table_name} - {table.capacity}
+              </option>
+            );
+          })}
+        </select>
+        <button
+          type="button"
+          className="btn btn-secondary m-3"
+          onClick={cancelHandler}
+        >
+          Cancel
+        </button>
+        <button type="submit" className="btn btn-primary m-3">
+          Submit
+        </button>
+      </form>
+      <ErrorAlert error={reservationError || tablesError || error} />
     </div>
   );
 }
