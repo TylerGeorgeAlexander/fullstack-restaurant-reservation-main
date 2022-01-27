059dcf3b6a279097274d34427c17852d9d0a8a72
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-27 08:28:26 -0800

    added finishReservation as a delete request

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -113,3 +113,14 @@ export async function updateTable(table_id, reservation_id) {
   };
   return await fetchJson(url, options, {});
 }
+
+export async function finishReservation(table_id, reservation_id, signal) {
+  const url = `${API_BASE_URL}/tables/${table_id}/seat`;
+  const options = {
+    method: "DELETE",
+    headers,
+    body: JSON.stringify({ data: { reservation_id }}),
+    signal,
+  };
+ return await fetchJson(url, options, signal);
+}
