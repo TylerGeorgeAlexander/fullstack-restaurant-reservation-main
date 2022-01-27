b0249fcacd9a1505905468ff5d501bd7a3b4e7d7
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-01-27 10:40:52 -0800

    popup window and wrote useEffect to handle refresh

--- front-end/src/dashboard/TablesTable.js
+++ front-end/src/dashboard/TablesTable.js
@@ -1,58 +1,81 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { finishReservation } from "../utils/api";
-import { useParams, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { listTables } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
 
-export default function TablesTable({ tables }) {
+export default function TablesTable() {
   const history = useHistory();
-  const [error, setError] = useState(null);
+  // const [error, setError] = useState(null);
+  const [tables, setTables] = useState([]);
+  const [tablesError, setTablesError] = useState(null);
 
-  function finishHandler(event) {
+  useEffect(loadDashboard, [tables]);
+
+  function loadDashboard() {
+    const abortController = new AbortController();
+    listTables(abortController.signal).then(setTables).catch(setTablesError);
+    return () => abortController.abort();
+  }
+
+  async function finishHandler(event) {
     event.preventDefault();
     // console.log(event.target.value)
-    finishReservation(event.target.value)
-      .then(() => {
-        history.go(0)
-      })
-      .catch(setError);
+
+    // Pop up window
+    if (
+      window.confirm(
+        "Is this table ready to seat new guests? This cannot be undone."
+      )
+    ) {
+      finishReservation(event.target.value).then(() => history.push("/"));
+    }
+    // To remove the error not being displayed react warning, may move to display later
+    // console.log(error);
   }
 
   return (
-    <table className="table table-striped table-dark">
-      <thead>
-        <tr>
-          <th scope="col">Table ID #</th>
-          <th scope="col">Table Name</th>
-          <th scope="col">Capacity</th>
-          <th scope="col">Free?</th>
-          <th scope="col">Button</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tables.map((table) => {
-          return (
-            <tr key={table.table_id}>
-              <th key={table.table_id} scope="row">
-                {table.table_id}
-              </th>
-              <td>{table.table_name}</td>
-              <td>{table.capacity}</td>
-              <td>{table.reservation_id ? "Occupied" : "Free"}</td>
-              <td>
-                {!table.reservation_id || (
-                  <button
-                    onClick={finishHandler}
-                    className="btn btn-danger"
-                    data-table-id-finish={table.table_id}
-                    value={table.table_id}
-                  >
-                    Finish
-                  </button>
-                )}
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div>
+      <table className="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Table ID #</th>
+            <th scope="col">Table Name</th>
+            <th scope="col">Capacity</th>
+            <th scope="col">Free?</th>
+            <th scope="col">Button</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tables.map((table) => {
+            return (
+              <tr key={table.table_id}>
+                <th key={table.table_id} scope="row">
+                  {table.table_id}
+                </th>
+                <td>{table.table_name}</td>
+                <td>{table.capacity}</td>
+                <td data-table-id-status={table.table_id}>
+                  {table.reservation_id ? "occupied" : "free"}
+                </td>
+                <td>
+                  {!table.reservation_id || (
+                    <button
+                      onClick={finishHandler}
+                      className="btn btn-danger"
+                      data-table-id-finish={table.table_id}
+                      value={table.table_id}
+                    >
+                      Finish
+                    </button>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <ErrorAlert error={tablesError} />
+    </div>
   );
 }
