779f3bad4b41671875dbf4fff942ee02813fd182
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-02-02 13:52:10 -0800

    modified list in service and added search

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -10,22 +10,30 @@ function create(newReservation) {
 async function list(reservation_date) {
   return knex("reservations")
     .select("*")
-    .orderBy("reservation_time")
-    .where({ reservation_date });
+    .where({ reservation_date })
+    .whereNot({ status: "finished" })
+    .whereNot({ status: "cancelled" })
+    .orderBy("reservation_time");
 }
 
-async function getReservationById(reservation_id){
-  return knex("reservations")
-  .select("*")
-  .where({ reservation_id })
-  .first(); 
+async function getReservationById(reservation_id) {
+  return knex("reservations").select("*").where({ reservation_id }).first();
 }
 
 function changeStatus(status, reservation_id) {
   return knex("reservations")
     .whereRaw(`reservation_id=${reservation_id}`)
-    .update({status})
-    .returning("status")
+    .update({ status })
+    .returning("status");
+}
+
+function search(mobile_number) {
+  return knex("reservations")
+    .whereRaw(
+      "translate(mobile_number, '() -', '') like ?",
+      `%${mobile_number.replace(/\D/g, "")}%`
+    )
+    .orderBy("reservation_date");
 }
 
 module.exports = {
@@ -33,4 +41,5 @@ module.exports = {
   list,
   getReservationById,
   changeStatus,
+  search,
 };
